e18ae289f3406203ac9685d5018b3e7630d2f6ab
Hung Bui <hungbui7690@gmail.com>
2024-11-20 21:15:43 -0700

    Add Search bar to filter products

--- app.ts
+++ app.ts
@@ -1,14 +1,6 @@
 /*
-  show products 
-    - fetch data from db.json
-    - show categories
-    - show products
-  
-
----------------------
-
-  1. app/page.js
-  2. components/Product.jsx
+  Search Bar
+  - 
 
 
 
